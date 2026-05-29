09431d1466b95eef95949d14d6c608f88673b02f
Fumihito TSUJIKAWA <readme627@gmail.com>
2026-05-29 23:35:36 +0900

    初期設定ファイルと基本的なアプリケーションおよびテストクラスを追加

---
+++ src/main/java/dev/happyfactory/App.java
@@ -0,0 +1,13 @@
+package dev.happyfactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
